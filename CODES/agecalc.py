60231c81acc9953894af272db14a5dfb13379669
MAYANK2454-19 <mk2599599@gmail.com>
2026-01-20 19:20:47 +0530

    First Python Code

--- CODES/agecalc.py
+++ CODES/agecalc.py
@@ -1,3 +1,5 @@
+'''An age calculator that takes your current age as input and calculates assuming maximum possible limit of your age as 90 years 
+to give remaining dyas , months and weeks as output '''
 age = input("What is Your Current age ? \n")
 left = 90 - int(age)
 months = int (left) * 12
