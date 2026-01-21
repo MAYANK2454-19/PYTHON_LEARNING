7dc07e8731078599e13f4a25e53b8f272a40e76d
MAYANK2454-19 <mk2599599@gmail.com>
2026-01-21 16:11:21 +0530

    ODD OR EVEN NUMBER

---
+++ CODES/oddeven.py
@@ -0,0 +1,6 @@
+#Check wether the given number is odd or even using the modulo operator (%)
+n = int (input("Enter Your Number of Choice ? \n"))
+if n % 2 == 0 :
+    print("It is an even number")
+else :
+    print("It is an odd number")
