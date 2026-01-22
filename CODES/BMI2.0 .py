868d18e7923bf2c193218df6bdb96a87877a256f
MAYANK2454-19 <mk2599599@gmail.com>
2026-01-22 21:23:48 +0530

    few changes in bmi calc

--- CODES/BMI2.0 .py
+++ CODES/BMI2.0 .py
@@ -2,14 +2,14 @@ height = float (input("What is your height ? \n"))
 weight = float (input("What is your weight ? \n"))
 bmi_calc= (weight)/height ** 2
 bmi = round(bmi_calc , 2)
-print(f"Your BMI is {bmi}")
+# print(f"Your BMI is {bmi}")
 if bmi < 18.5 :
-    print("You are Underweight !")
+    print(f"Your BMI is {bmi} , You are Underweight !")
 elif 18.5 <= bmi < 25 :
-    print("You are Normal weight !")
+    print(f"Your BMI is {bmi} , You are Normal weight !")
 elif 25 <= bmi < 30 :
-    print("Your are Overweight !")
+    print(f"Your BMI is {bmi} , Your are Overweight !")
 elif 30 <= bmi < 35 :
-    print("Your are Obese !")
+    print(f"Your BMI is {bmi} , Your are Obese !")
 else :
-    print("You are Clinically Obese")
+    print(f"Your BMI is {bmi} , You are Clinically Obese")
