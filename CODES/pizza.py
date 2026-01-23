263fac67a6a3b0bd341d9a1c8883a41329a43e4e
MAYANK2454-19 <mk2599599@gmail.com>
2026-01-23 11:22:06 +0530

    few changes

--- CODES/pizza.py
+++ CODES/pizza.py
@@ -1,6 +1,13 @@
 #A pizza order calculator that calculates the final bill on  the basis of selcetions by the customer
 
 print("Welcome ! to Python Pizza Deliveries\n")
+print("Small = $15")
+print("Medium = $20")
+print("Large = $25")
+print("Pepperoni for small = $2")
+print("Pepperoni for medium and large = $3")
+print("Extra cheese = $1")
+
 size = input("Which size Pizza do you want ? S , M , L \n")
 add_pepperoni = input("Do you want extra Pepproni ? Y or N \n")
 extra_cheese = input("Do you want extra cheese ? Y or N \n")
@@ -33,4 +40,4 @@ else :
 print(f"Size = {size}")
 print(f"Pepperoni = {add_pepperoni}")
 print(f"Extra Cheese = {extra_cheese}\n")
-print(f"Your final bill is {bill}\n")
+print(f"Your final bill is ${bill}\n")
