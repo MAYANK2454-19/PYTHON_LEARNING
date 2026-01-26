7e63305dba93f7b0c5e212a1b19ccce917c0caa2
MAYANK2454-19 <mk2599599@gmail.com>
2026-01-26 22:58:00 +0530

    updated the code using the choice function

--- CODES/paythebill.py
+++ CODES/paythebill.py
@@ -1,6 +1,8 @@
 import random 
 names_string = input("Give me names of everyone , separated with a comma ")
-names=names_string.split(", ")
-num = int (len(names))
-a = random.randint(0,(num-1))
-print(f"{names[a]} is going to pay the bill.")
+names=names_string.split(",")
+# num = int (len(names))
+# a = random.randint(0,(num-1))
+# using the choice function
+a=random.choice(names)
+print(f"{a} is going to pay the bill.")
